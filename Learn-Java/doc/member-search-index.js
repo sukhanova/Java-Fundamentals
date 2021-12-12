a405b8a856ea08a44bf38fd28234ce68ca6a16be
sukhanova <verusia.s@gmail.com>
2021-12-11 18:07:52 -0600

    Added comments and generated javadoc

---
+++ Learn-Java/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.gitbulls.sukhanova","c":"HelloWorld","l":"HelloWorld()","u":"%3Cinit%3E()"},{"p":"com.gitbulls.sukhanova","c":"HelloWorld","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- Learn-Java/src/com/gitbulls/sukhanova/HelloWorld.java
+++ Learn-Java/src/com/gitbulls/sukhanova/HelloWorld.java
@@ -1,9 +1,26 @@
 package com.gitbulls.sukhanova;
 
+
 public class HelloWorld {
 
+	/**
+	 * The entry point to the program.
+	 * @param args input arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		// TODO add more code here in future
+		//To find all TODO comments go to Window > Show View > Tasks here
+		// Single comment
+		
+		/* 
+		 * Multi 
+		 * line
+		 * comment
+		 * */
+		
+		
 	}
 
 }
