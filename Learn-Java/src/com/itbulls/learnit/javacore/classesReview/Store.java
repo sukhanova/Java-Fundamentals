dafb9dba51dccfe7334a7e4f92737a0285b01edd
sukhanova <verusia.s@gmail.com>
2021-12-23 16:43:41 -0600

    review Classes

---
+++ Learn-Java/src/com/itbulls/learnit/javacore/classesReview/Store.java
@@ -0,0 +1,27 @@
+package com.itbulls.learnit.javacore.classesReview;
+
+public class Store {
+	// instance fields
+	  String productType;
+	  int inventoryCount;
+	  double inventoryPrice;
+	  
+	  // constructor method
+	  public Store(String product, int count, double price) {
+	    productType = product;
+	    inventoryCount = count;
+	    inventoryPrice = price;
+	  }
+	  
+	  // main method
+	  public static void main(String[] args) {
+	    Store cookieShop = new Store("cookies", 12, 3.75);
+	    System.out.println(cookieShop);
+	    System.out.println(cookieShop.productType);
+	    System.out.println(cookieShop.inventoryCount);
+	    System.out.println(cookieShop.inventoryPrice);
+	    System.out.println("Our store specials are " +cookieShop.productType +". So far we can offer " +cookieShop.inventoryCount + " "+
+	    		cookieShop.productType + " and price is " + cookieShop.inventoryPrice +" for each.");
+	  }
+
+}
